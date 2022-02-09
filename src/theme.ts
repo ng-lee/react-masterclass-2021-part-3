da5f3d504e5b1ca3fe5654ef8e224c8ead3d386c
codeer <loginonlyyy@gmail.com>
2022-02-09 20:43:21 +0900

    minutes to hours using get selector

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
   textColor: "#f5f6fa",
+  bgColor: "#2f3640",
   accentColor: "#4cd137",
 };
